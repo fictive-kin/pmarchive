b08077cc68817e07f0a74a56ed00d55bd77319f5
Blake Yoder <yoderblake@gmail.com>
2017-08-24 15:32:26 -0400

    Remove unnecessary route and folder

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -10,11 +10,6 @@ def index():
 @app.route('/startups')
 def startups():
     return render_template('startups.html')
-
-
-@app.route('/startups/<path:page>')
-def startup_category(page):
-    return send_from_directory('pages/startups', page)
 
 
 if __name__ == '__main__':
