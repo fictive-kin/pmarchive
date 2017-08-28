74d5ce1cbb5b6503e0cb9df05cbb69803a7b651c
Briana Camp <brianarcamp@gmail.com>
2017-08-28 10:11:32 -0700

    style the mobile version of the chapter sidebar and dynamically add sidebar chapters based on chapters in page ref #5

---
+++ static/js/chapter-sidebar.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+
+    var chapterSections = document.querySelectorAll('.chapter');
+    var sidebar = document.querySelector('.chapter-list');
+    var counter = 1;
+
+    for (var i = 0; i < chapterSections.length; i++) {
+
+        //create li and anchor elements for sidebar
+        var li = document.createElement('li');
+        li.setAttribute('class', 'chapter-list__item');
+
+        var chapterLink = document.createElement('a');
+        chapterLink.setAttribute('class', 'chapter-list__link');
+
+        var chapterNum = '#chapter-' + counter;
+        chapterLink.setAttribute('href', chapterNum);
+
+        chapterLink.textContent = counter;
+
+        //append elements to dom
+        sidebar.appendChild(li);
+        li.appendChild(chapterLink);
+
+        counter++;
+    }
+
+});
