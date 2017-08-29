4ceab4922c53a9a172bbb410ea3d6392d3ea40c1
Briana Camp <brianarcamp@gmail.com>
2017-08-29 16:08:25 -0700

    adds accessibility to nav, enhances animation on nav ref #6

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(event){
-    var navEl = document.querySelector('.nav');
+    var bodyEl = document.querySelector('.body');
     var hamburgerEl = document.querySelector('.hamburger-container');
+    var navList = document.querySelector('.site-nav__link');
 
     hamburgerEl.addEventListener('click', function(){
-        navEl.classList.toggle('js-is-active');
+        bodyEl.classList.toggle('js-is-active');
+    });
+
+    //for accessibility
+    hamburgerEl.addEventListener('keypress', function(e){
+        //if user presses the enter key toggle class
+        if (e.keyCode == 13) {
+            bodyEl.classList.toggle('js-is-active');
+        }
+    });
+
+    navList.addEventListener('keydown', function(e) {
+        //if user presses the escape key while they are in the nav
+        if (e.keyCode == 27) {
+            bodyEl.classList.remove('js-is-active');
+        }
     });
 });
